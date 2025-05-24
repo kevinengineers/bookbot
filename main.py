a9c42200a684cd98dc8b12284b1a18717dadf8f6
kevinengineers <kevinengineers@proton.me>
2025-05-24 10:46:00 -0700

    removed hardcoded paths to /books/name_of_book imported sys and used to ask user for path to book to run, prints usage if only 1 arg is supplied

--- main.py
+++ main.py
@@ -15,16 +15,21 @@ def pretty_print_dict(list_of_dicts):
 
 
 def main():
-    book_str = get_book_text("./books/frankenstein.txt")
-    num_of_words = word_count(book_str)
-    dict_of_words = char_count(book_str)
-    char_list = sort_dict(dict_of_words)
-    book = "./books/frankenstein.txt"
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book}")
-    print("------------ Word Count ------------")
-    print(f"Found { num_of_words } total words")
-    print("--------- Character Count -------")
-    pretty_print_dict(char_list)    
-    print("============= END ===============")
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_str = get_book_text(sys.argv[1])
+        num_of_words = word_count(book_str)
+        dict_of_words = char_count(book_str)
+        char_list = sort_dict(dict_of_words)
+        book = "./books/frankenstein.txt"
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book}")
+        print("------------ Word Count ------------")
+        print(f"Found { num_of_words } total words")
+        print("--------- Character Count -------")
+        pretty_print_dict(char_list)    
+        print("============= END ===============")
+
 main()
